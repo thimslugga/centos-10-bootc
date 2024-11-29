c177f51c318a20a5a26ce631aa43fb11cad97e29
Charles <c4a@monocles.eu>
2024-11-29 15:37:22 -0500

    Update entrypoint.sh

--- iso/9/files/entrypoint.sh
+++ iso/9/files/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -Lo /upstream.iso https://repo.almalinux.org/almalinux/9.5/BaseOS/x86_64/os/images/boot.iso
+curl -Lo /upstream.iso https://repo.almalinux.org/almalinux/9/BaseOS/x86_64/os/images/boot.iso
 rm -vf /output/upstream.iso || true
 mkksiso --ks /ks.ks /upstream.iso /output/9.iso
