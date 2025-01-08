3a6ae0281ab04f2a3816dcaa17f0e796c4ce9e34
Charles <c4a@monocles.eu>
2025-01-07 20:06:56 -0500

    Update README + CI + ISO

--- iso/entrypoint.sh
+++ iso/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-curl -Lo /upstream.iso https://mirror.stream.centos.org/10-stream/BaseOS/x86_64/os/images/boot.iso
-mkksiso /kickstart /upstream.iso /output/calcite-10.iso
+cd /
+curl -Lo upstream.iso https://mirror.stream.centos.org/10-stream/BaseOS/x86_64/os/images/boot.iso
+mkksiso -a container kickstart upstream.iso output/calcite-10.iso
+cd -
